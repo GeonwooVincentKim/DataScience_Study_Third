dbe0ecdbe86c0be69a8d6e757c43fe11f956d166
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-05 21:32:23 +0900

    [Feature] - Linear-Regresssion

--- gradient-descent-example.py
+++ gradient-descent-example.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from vector.vector_example import Vector, dot
 
 
@@ -6,3 +7,23 @@ def sum_of_squares(v: Vector):
         Calculate the belong items of `v` with `Sum of Squares` 
     """
     return dot(v, v)
+
+
+def difference_quotient(
+    f: Callable[[float], float],
+    x: float,
+    h: float
+):
+    return (f(x + h) - f(x)) / h 
+
+
+def square(x: float):
+    return x * x
+
+
+def derivative(x: float):
+    return 2 * x
+
+
+xs = range(-10, 11)
+print("XS -> {0}".format(xs))
