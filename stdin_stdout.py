bae5db63a8d6218b31041f839c63e0ee1d3e31b4
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-19 21:52:52 +0900

    [Update] - Just testing

--- stdin_stdout.py
+++ stdin_stdout.py
@@ -11,3 +11,9 @@ for line in sys.stdin:
     count += 1
 
 print(count)
+
+a = input()
+
+for i in range(a):
+    b, c = map(int, input().split(" "))
+    print("Case: #{0}: {1}".format((i + 1), b + c))
