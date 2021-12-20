091e1ba5c331de525fb2ffab1232687dd1d21389
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-20 21:16:57 +0900

    [Feature] - Display Tab-Delimited-Stock-Prices result

--- stdin_stdout.py
+++ stdin_stdout.py
@@ -1,19 +1,45 @@
 import sys, re
+import os
+import csv
 
-regex = sys.argv[0]
+sys.path.append(os.path.join(os.path.dirname(os.path.join(os.path.dirname(__file__)))))
 
-for line in sys.stdin:
-    if re.search(regex, line):
-        sys.stdout.write(line)
+from File.FileManager import *
 
-count = 0
-for line in sys.stdin:
-    count += 1
+# regex = sys.argv[0]
 
-print(count)
+# for line in sys.stdin:
+#     if re.search(regex, line):
+#         sys.stdout.write(line)
 
-a = input()
+# count = 0
+# for line in sys.stdin:
+#     count += 1
 
-for i in range(a):
-    b, c = map(int, input().split(" "))
-    print("Case: #{0}: {1}".format((i + 1), b + c))
+# print(count)
+
+# a = input()
+
+# for i in range(a):
+#     b, c = map(int, input().split(" "))
+#     print("Case: #{0}: {1}".format((i + 1), b + c))
+
+print("Tab Delimited Stock Price")
+
+
+def process(date, symbol, price):
+    print(date, symbol, price)
+
+
+
+if __name__ == "__main__":
+    with open("tab_delimited_stock_prices.txt") as f:
+        tab_reader = csv.reader(f, delimiter="\t")
+        print("Tab Reader -> {0}".format(tab_reader))
+
+        for row in tab_reader:
+            date = row[0]
+            symbol = row[1]
+            closing_price = float(row[2])
+
+            process(date, symbol, closing_price)
