90d96d8eb9c6107fe411242a5707ba382a81d892
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-25 22:53:21 +0900

    [Feature] - Get All URLs from External URLS

--- crawl_data.py
+++ crawl_data.py
@@ -4,5 +4,15 @@ import requests
 
 url = "https://www.house.gov/representatives"
 text = requests.get(url).text
+soup = BeautifulSoup(text, "html5lib")
 
 print("Get URL Text -> {0}".format(text))
+
+all_urls = [
+    a['href']
+    for a in soup('a')
+    if a.has_attr('href')
+]
+
+print(len(all_urls))
+print("All URLs -> {0}".format(all_urls))
