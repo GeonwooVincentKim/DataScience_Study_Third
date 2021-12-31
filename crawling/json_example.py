b999c1ded47d0ccea8b996aad10048f78ef11241
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-31 22:26:30 +0900

    [Feature] - Wrote JSON File again

--- crawling/json_example.py
+++ crawling/json_example.py
@@ -17,7 +17,7 @@ print("Deserialized Topics -> {0}".format("data science" in deserialized["topics
 
 
 # Write `JSON` Data (file)
-with open("json_data.txt", "w") as json_file:
+with open("json_data.json", "w") as json_file:
     json.dump(serialized, json_file)
 
 # Get `JSON` data and write `XML` file
