236e33230f166ae31dca1020564f95aabf3e8b76
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-31 22:19:14 +0900

    [Feature] - Writing JSON File

--- crawling/json_example.py
+++ crawling/json_example.py
@@ -1,4 +1,7 @@
 import json
+import json2xml
+import xml
+
 
 serialized = """{
     "title": "Data Science Book",
@@ -11,3 +14,11 @@ deserialized = json.loads(serialized)
 print("Deserialized -> {0}".format(deserialized))
 print("Deserialized Publication-Year -> {0}".format(deserialized["publicationYear"]))
 print("Deserialized Topics -> {0}".format("data science" in deserialized["topics"]))
+
+
+# Write `JSON` Data (file)
+with open("json_data.txt", "w") as json_file:
+    json.dump(serialized, json_file)
+
+# Get `JSON` data and write `XML` file
+
