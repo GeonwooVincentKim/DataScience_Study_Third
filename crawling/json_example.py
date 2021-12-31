384e25e8a32f09c789e05f0b1a159cb00e04b6a5
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-31 22:11:57 +0900

    [Update] - Change variable name

---
+++ crawling/json_example.py
@@ -0,0 +1,12 @@
+import json
+
+serialized = """{
+    "title": "Data Science Book",
+    "author": "Joel Grus",
+    "publicationYear": 2019,
+    "topics": ["data", "science", "data science"]
+}"""
+
+deserialized = json.loads(serialized)
+print("Deserialized -> {0}".format(deserialized))
+print("Deserialized Publication-Year -> {0}".format(deserialized["publicationYear"]))
