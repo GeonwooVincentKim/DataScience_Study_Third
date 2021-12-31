bb6a1bc652e61993991b83010f2f8fd50787bc96
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-31 22:29:24 +0900

    [Feature] - Created XML example.py

--- crawling/xml_example.py
+++ crawling/xml_example.py
@@ -4,4 +4,7 @@ import json
 
 with open("json_data.json") as json_data:
     get_json = json.load(json_data)
+    print("{0}".format(get_json))
 
+read_xml = xml_e_tree.Element("book_information")
+print("XML Element Name -> {0}".format(read_xml))
