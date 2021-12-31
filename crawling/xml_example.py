f03840c1811e41369dbaf72a450a7bd358aee67a
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-31 22:49:45 +0900

    [Feature] - Applied JSON Codes to the XML file status

--- crawling/xml_example.py
+++ crawling/xml_example.py
@@ -2,9 +2,15 @@ import xml
 import xml.etree.cElementTree as xml_e_tree
 import json
 
+
+# Get `JSON` data and write `XML` file
 with open("json_data.json") as json_data:
     get_json = json.load(json_data)
     print("{0}".format(get_json))
 
 read_xml = xml_e_tree.Element("book_information")
 print("XML Element Name -> {0}".format(read_xml))
+
+xml_e_tree.SubElement(read_xml, "title").text = get_json["title"]
+xml_e_tree.SubElement(read_xml, "author").text = get_json["author"]
+xml_e_tree.SubElement(read_xml, "publicationYear").text = str(get_json["publicationYear"])
