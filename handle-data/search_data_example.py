2437087199c3ac0b9a36c2881c81631dce402c49
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-01 22:34:56 +0900

    [Feature] - Make HIstogram

--- handle-data/search_data_example.py
+++ handle-data/search_data_example.py
@@ -11,3 +11,10 @@ def bucketize(point: float, bucket_size: float):
         multiple time of `bucket_size`
     """
     return bucket_size * math.floor(point / bucket_size)
+
+def make_histogram(points: List[float], bucket_size: float):
+    """
+        Generate the section, and then calculate the numbers of each data
+        of each section
+    """
+    return Counter(bucketize(point, bucket_size) for point in points)
