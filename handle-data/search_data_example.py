24a9123e33779c5f77930d7990f5d3c18ef718c5
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-01 22:37:19 +0900

    [Feature] - Generate the function for Plot-Histogram

--- handle-data/search_data_example.py
+++ handle-data/search_data_example.py
@@ -12,9 +12,17 @@ def bucketize(point: float, bucket_size: float):
     """
     return bucket_size * math.floor(point / bucket_size)
 
+
 def make_histogram(points: List[float], bucket_size: float):
     """
         Generate the section, and then calculate the numbers of each data
         of each section
     """
     return Counter(bucketize(point, bucket_size) for point in points)
+
+
+def plot_histogram(points: List[float], bucket_size: float, title: str=""):
+    histogram = make_histogram(points, bucket_size)
+    
+    plt.bar(histogram.keys(), histogram.values(), width=bucket_size)
+    plt.title(title)
