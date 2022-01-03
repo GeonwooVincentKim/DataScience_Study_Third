9e51e2c173e626b71383aedba59aed73ecac5b85
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-03 21:03:09 +0900

    [Feature] - Added Comments

--- handle-data/named_tuple.py
+++ handle-data/named_tuple.py
@@ -1,5 +1,7 @@
 import datetime
 
+# Using Dictionary-Style
+# It could occurs lots of error by mistype the variable or some codes
 stock_price = {
     'closing_price': 102.06,
     'date': datetime.date(2014, 8, 29),
