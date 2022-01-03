4663c5624ce8c515fe7e3286295648d6a25a2b30
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-03 21:15:55 +0900

    [Feature] - StockPrice

--- handle-data/named_tuple.py
+++ handle-data/named_tuple.py
@@ -1,7 +1,9 @@
 import datetime
+from collections import namedtuple
 
 # Using Dictionary-Style
 # It could occurs lots of error by mistype the variable or some codes
+print("<Use Dictionary only>")
 stock_price = {
     'closing_price': 102.06,
     'date': datetime.date(2014, 8, 29),
@@ -10,3 +12,12 @@ stock_price = {
 
 for price in stock_price:
     print("Info -> {0}".format(price))
+
+print("\n--------------------\n")
+print("<Use Named-Tuple>")
+
+StockPrice = namedtuple('StockPrice', ['symbol', 'date', 'closing_price'])
+price = StockPrice('MSFT', datetime.date(2018, 12, 14), 106.03)
+
+print("Price - Symbol -> {0}".format(price.symbol))
+print("Price - Closing-Price -> {0}".format(price.closing_price))
