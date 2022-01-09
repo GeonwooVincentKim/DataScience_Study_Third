9b0ed596795609ceb4a4e567e3e9e3d3988cb6f1
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-09 18:06:31 +0900

    [Update] - Show current data information

--- handle-data/handle_tuple_data.py
+++ handle-data/handle_tuple_data.py
@@ -2,5 +2,11 @@ from named_tuple import *
 
 
 data = [
-    
+    StockPrice(
+        symbol='MSFT',
+        date=datetime.date(2018, 12, 24),
+        closing_price=106.03
+    )
 ]
+
+print("Current Data Information -> {0}".format(data))
