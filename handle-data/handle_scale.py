5d51e2bc69b5834e474eb7515a78d7ec448dcec0
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-16 20:00:40 +0900

    [Feature] - Getting values

--- handle-data/handle_scale.py
+++ handle-data/handle_scale.py
@@ -5,12 +5,21 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from vector_example import distance
 
 
-print("\n")
+print("\n-------------------------------\n")
 
 a_to_b = distance([63, 150], [67, 160])
 a_to_c = distance([63, 150], [70, 171])
 b_to_c = distance([67, 160], [70, 171])
 
-print("A to B -> {0}".format(a_to_b))
-print("B to C -> {0}".format(a_to_c))
-print("B to C -> {0}".format(a_to_c))
+print("A to B (1st) -> {0}".format(a_to_b))
+print("B to C (1st) -> {0}".format(a_to_c))
+print("B to C (1st) -> {0}".format(a_to_c))
+
+a_to_b = distance([160, 150], [170.2, 160])
+a_to_c = distance([160, 150], [177.8, 171])
+b_to_c = distance([170.2, 160], [177.8, 171])
+
+print("\n-------------------------------\n")
+print("A to B (2nd) -> {0}".format(a_to_b))
+print("B to C (2nd) -> {0}".format(a_to_c))
+print("B to C (2nd) -> {0}".format(a_to_c))
